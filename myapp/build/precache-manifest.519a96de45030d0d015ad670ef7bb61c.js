1cee8937918eea7ead41a252b8e99eae995407e9
Meena <meena@auctionsoftware.com>
2019-09-07 15:51:54 +0530

    new

---
+++ myapp/build/precache-manifest.519a96de45030d0d015ad670ef7bb61c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e331803b17d754d2bcf6d94aab877e08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a436c98536fca6769530",
+    "url": "/static/css/main.fb059932.chunk.css"
+  },
+  {
+    "revision": "bbf82fb0bf60ee8890ec",
+    "url": "/static/js/2.fe1e875f.chunk.js"
+  },
+  {
+    "revision": "a436c98536fca6769530",
+    "url": "/static/js/main.b896540e.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
