5117b7a2f901dd945fbd1c0cff996b01e721d367
Meena <meena@auctionsoftware.com>
2019-09-07 15:10:04 +0530

    new

---
+++ myapp/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+})
+
+//start server
+app.listen(port, (req, res) => {
+  console.log(`server listening on port: ${port}`);
+})
