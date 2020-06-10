ba5b832f562762a698ac270c3e2053a8c00c513b
Rajashekar Reddy Busi Reddy <rajashekar@ilancecustomization.com>
2020-06-10 18:25:18 +0530

    Meena - driverin

---
+++ projects/driverin/html/js/driverin-script.js
@@ -0,0 +1,103 @@
+jQuery("#banner-carousel").owlCarousel({
+	autoplay: false,
+	items: 1,
+	loop: true,
+	responsiveClass: true,
+	autoHeight: true,
+	autoplayTimeout: 10000,
+	smartSpeed: 800,
+	nav: false
+	/*  , responsive: {
+	      0: {
+	          items: 1
+	      },
+
+	      600: {
+	          items: 3
+	      },
+
+	      1024: {
+	          items: 4
+	      },
+
+	      1366: {
+	          items: 4
+	      }
+	  } */
+});
+jQuery("#features-carousel").owlCarousel({
+	autoplay: false,
+	items: 1,
+	loop: true,
+	animateOut: 'fadeOut',
+	animateIn: 'fadeIn',
+	responsiveClass: true,
+	autoHeight: true,
+	autoplayTimeout: 10000,
+	smartSpeed: 800,
+	nav: false
+
+});
+jQuery("#cityride-carousel").owlCarousel({
+	autoplay: false,
+	items: 3,
+	loop: true,
+	responsiveClass: true,
+	autoHeight: true,
+	autoplayTimeout: 10000,
+	smartSpeed: 800,
+	nav: false
+
+});
+jQuery("#usecase-carousel").owlCarousel({
+	autoplay: false,
+	items: 1,
+	margin: 15,
+	animateOut: 'fadeOut',
+	animateIn: 'fadeIn',
+	loop: true,
+	responsiveClass: true,
+	autoHeight: true,
+	autoplayTimeout: 10000,
+	smartSpeed: 800,
+	nav: false
+
+});
+jQuery("#customer-carousel").owlCarousel({
+	autoplay: false,
+	items: 1,
+	loop: true,
+	responsiveClass: true,
+	autoHeight: true,
+	autoplayTimeout: 10000,
+	smartSpeed: 800,
+	nav: true,
+	navText: ["<img src='images/left-arrow.jpg'>", "<img src='images/right-arrow.jpg'>"]
+
+});
+jQuery("#members-carousel").owlCarousel({
+	autoplay: false,
+	items: 4,
+	loop: true,
+	responsiveClass: true,
+	autoHeight: true,
+	autoplayTimeout: 10000,
+	smartSpeed: 800,
+	nav: false
+
+});
+jQuery("#honours-carousel").owlCarousel({
+	autoplay: false,
+	items: 6,
+	loop: true,
+	responsiveClass: true,
+	autoHeight: true,
+	autoplayTimeout: 10000,
+	smartSpeed: 800,
+	nav: false
+
+});
+AOS.init({
+  duration: 1200,
+});
+ 
