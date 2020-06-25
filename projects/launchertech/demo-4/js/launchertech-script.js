d74c8892fc3d088f64d77cad87447e0af45115fa
meena2006 <ecemuthumeena@gmail.com>
2020-06-25 14:23:15 +0530

    Meena - new changes

---
+++ projects/launchertech/demo-4/js/launchertech-script.js
@@ -0,0 +1,65 @@
+ 
+jQuery("#testimonials-carousel").owlCarousel({
+	autoplay: true,
+	items: 1,
+	loop: true,
+	responsiveClass: true,
+	autoHeight: true,
+	autoplayTimeout: 10000,
+	smartSpeed: 800,
+	nav: false,
+	//navText: ["<img src='images/left-arrow.jpg'>", "<img src='images/right-arrow.jpg'>"]
+
+});
+
+$('#custCarousel').carousel({ interval: 6000 });
+ 
+AOS.init({
+  easing: 'ease-out-back',
+  duration: 1000
+});
+ 
+ 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 80) {
+      $('.fixed-top').addClass('header-scrolled');
+        $('body').addClass('scrolled');
+    } else {
+      $('.fixed-top').removeClass('header-scrolled');
+        $('body').removeClass('scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 80) {
+    $('.fixed-top').addClass('header-scrolled');
+  }
+  
+
+//			$('.down-arr').on('click', function(e) {
+//				$('html, body').animate({
+//					scrollTop: $(window).height()
+//				}, 1200);
+//			});
+//$("a[href^='#']").click(function(e) {
+//	e.preventDefault();
+//	
+//	var position = $($(this).attr("href")).offset().top - 70;
+//
+//	$("body, html").animate({
+//		scrollTop: position
+//	});
+//});
+
+ $(function()  {
+        $('.full-page-slider').fullpage({
+          navigation: true,
+          navigationPosition: 'right',
+          css3: true,
+          scrollingSpeed: 1000,
+          easingcss3: 'cubic-bezier(0.86, 0, 0.07, 1)'
+        });
+
+        $('.bounce').click(function() {
+          $.fn.fullpage.moveSectionDown();
+        });
+          });
