7e1ce0727effc30a5c2b8f2fbdae4369e7584dc7
meena2006 <ecemuthumeena@gmail.com>
2020-12-30 19:32:14 +0530

    Meena - new changes

--- projects/tally/js/app.js
+++ projects/tally/js/app.js
@@ -1,67 +1,122 @@
-! function($) {
-    "use strict";
+// ! function($) {
+//     "use strict";
 
-    var TallyS = function() {};
+//     var TallyS = function() {};
 
-    // TallyS.prototype.initStickyMenu = function() {
-    //     $(window).scroll(function() {
-    //         var scroll = $(window).scrollTop();
+//     // TallyS.prototype.initStickyMenu = function() {
+//     //     $(window).scroll(function() {
+//     //         var scroll = $(window).scrollTop();
         
-    //         if (scroll >= 50) {
-    //             $(".sticky").addClass("nav-sticky");
-    //         } else {
-    //             $(".sticky").removeClass("nav-sticky");
-    //         }
-    //     });
-    // },
+//     //         if (scroll >= 50) {
+//     //             $(".sticky").addClass("nav-sticky");
+//     //         } else {
+//     //             $(".sticky").removeClass("nav-sticky");
+//     //         }
+//     //     });
+//     // },
 
-    TallyS.prototype.initSmoothLink = function() {
-        $('.navbar-nav a').on('click', function(event) {
+//     // TallyS.prototype.initSmoothLink = function() {
+//     //     $('.navbar-nav a').on('click', function(event) {
+//     //         var $anchor = $(this);
+//     //         $('html, body').stop().animate({
+//     //             scrollTop: $($anchor.attr('href')).offset().top - 0
+//     //         }, 1500, 'easeInOutExpo');
+//     //         event.preventDefault();
+//     //     });
+//     // },
+
+//     // TallyS.prototype.initScrollspy = function() {
+//     //     $("#navbarCollapse").scrollspy({
+//     //         offset:20
+//     //     });
+//     // },
+
+//     TallyS.prototype.initTesticarousel = function() {
+//         $('#testi-carousel').owlCarousel({
+//             items: 1,
+//             loop:true,
+//             margin:10,
+//             nav:true,
+//             responsive:{
+//                 576:{
+//                     items:1
+//                 },
+     
+//             }
+//         });
+//     },
+ 
+
+//     // feather.replace()
+
+//     TallyS.prototype.init = function() {
+//         // this.initStickyMenu();
+//         // this.initSmoothLink();
+//         // this.initScrollspy();
+//         this.initTesticarousel(); 
+//     },
+//     //init
+//     $.TallyS = new TallyS, $.TallyS.Constructor = TallyS
+// }(window.jQuery),
+
+
+// //initializing
+// function($) {
+//     "use strict";
+//     $.TallyS.init();
+// }(window.jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+   $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+        
+            if (scroll >= 50) {
+                $(".sticky").addClass("nav-sticky");
+            } else {
+                $(".sticky").removeClass("nav-sticky");
+            }
+        });
+
+
+
+
+$('.navbar-nav a').on('click', function(event) {
             var $anchor = $(this);
             $('html, body').stop().animate({
                 scrollTop: $($anchor.attr('href')).offset().top - 0
             }, 1500, 'easeInOutExpo');
             event.preventDefault();
         });
-    },
 
-    // TallyS.prototype.initScrollspy = function() {
-    //     $("#navbarCollapse").scrollspy({
-    //         offset:20
-    //     });
-    // },
 
-    TallyS.prototype.initTesticarousel = function() {
-        $('#testi-carousel').owlCarousel({
-            items: 1,
-            loop:true,
-            margin:10,
-            nav:true,
-            responsive:{
-                576:{
-                    items:1
-                },
-     
-            }
+
+$("#navbarCollapse").scrollspy({
+            offset:20
         });
-    },
- 
-
-    // feather.replace()
-
-    TallyS.prototype.init = function() {
-        // this.initStickyMenu();
-        this.initSmoothLink();
-        // this.initScrollspy();
-        this.initTesticarousel(); 
-    },
-    //init
-    $.TallyS = new TallyS, $.TallyS.Constructor = TallyS
-}(window.jQuery),
 
 
-//initializing
-function($) {
-    "use strict";
-    $.TallyS.init();
-}(window.jQuery);
+feather.replace()
+
+$('#testi-carousel').owlCarousel({
+    items: 1,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        992:{
+            items:2
+        },
+
+    }
+});
